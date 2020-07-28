55bd89a0135203de60431641874a0f04a81d2e61
Hu Tiger <huxy99@yahoo.com>
2020-07-28 16:33:54 -0500

    code CustomerService class

--- src/com/youhubs/customer/CustomerController.java
+++ src/com/youhubs/customer/CustomerController.java
@@ -1,5 +1,8 @@
 package com.youhubs.customer;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -7,10 +10,15 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class CustomerController {
 
+	@Autowired
+	private CustomerService service;
+
 	@RequestMapping("/")
 	public ModelAndView home() {
 		ModelAndView mv = new ModelAndView("index");
-		mv.addObject("message", "Hello from Spring MVC!");
+		List<Customer> customers = service.listCustomer();
+		mv.addObject("customers", customers);
+
 		return mv;
 	}
 
---
+++ src/com/youhubs/customer/CustomerService.java
@@ -0,0 +1,19 @@
+package com.youhubs.customer;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+
+	@Autowired
+	private CustomerRepository repo;
+
+	public List<Customer> listCustomer() {
+		return (List<Customer>) repo.findAll();
+	}
+	
+	
+}
